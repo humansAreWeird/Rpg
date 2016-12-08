405663321bcdd86da21c77226b417a9f5c7525ec
R <rulas164@gmail.com>
2016-12-08 00:44:18 -0600

    moving

---
+++ Rpg/Assets/_Scripts/PlayerMovement.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour {
+
+    private Rigidbody2D rb;
+    private Vector2 movement;
+
+    public float speed;
+
+	void Start () {
+        rb = GetComponent<Rigidbody2D>();
+	}
+	
+	void Update () {
+        movement = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical")).normalized * speed;
+	}
+
+    void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + movement * Time.fixedDeltaTime);
+    }
+}
